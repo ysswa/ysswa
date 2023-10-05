f03c4bd7499c44d9fc9a20f9b3c6b407e694a7a0
ysswa <113962577+ysswa@users.noreply.github.com>
2023-10-05 15:14:40 +0530

    Add files via upload

---
+++ String_operations.py
@@ -0,0 +1,31 @@
+#the uses of single court ,dounle court and triple courts in  python print statements
+print("hello world")
+print('hello world')
+print('''hello world''')
+
+#take an input from user
+string_1=input("enter any string here:")
+print(string_1)
+
+#string operations
+name="siddhartha nayak"
+
+#capitalize the string
+print(f"capiutalized string is {name.capitalize()}")
+
+#uppercase the string
+print(f"upper case the string is {name.upper()}")
+
+#lowercase the string
+print(f"lower case the string is {name.lower()}")
+
+#finding the character in a string
+print(f"finding index of an specified character in the string is {name.find('d')}")
+
+#replace the character in a string
+print(f"after replace operation the  string is {name.replace('s','g')}")
+
+
+
+
+
